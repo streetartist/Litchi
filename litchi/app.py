b9f9b1220aecac8776767a1ff4d873ccb3041b9f
streetartist <60282562+streetartist@users.noreply.github.com>
2021-02-02 17:55:41 +0800

    [+]Make a beta app.py

--- litchi/app.py
+++ litchi/app.py
@@ -1,1 +1,21 @@
-# App code go there.
+# App code goes there.
+class App:
+    def run(self):
+        elements = self.build()
+        html_begin = '''
+            <html>
+                <body>
+        '''
+        for element in elements:
+            html_begin += element.convert()
+        html = html_begin + '''
+            </body>
+        </html>
+        '''
+        return html
+        
+        app = Flask(__name__)
+
+        @app.route('/',methods=['GET','POST']):
+        def web():
+            pass
