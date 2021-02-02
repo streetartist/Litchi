4f4695e0149b1b3693ec7a3c906ea42c76c51e2b
streetartist <60282562+streetartist@users.noreply.github.com>
2021-02-02 17:59:40 +0800

    Update button.py

--- litchi/uix/button.py
+++ litchi/uix/button.py
@@ -1,1 +1,6 @@
-# Button code go there.
+# Button code goes there.
+class Button:
+    def __init__(self, text, on_pressed, hold):
+        self.text = text
+    def convert(self):
+        return "<button>{self.text}</button>"
