f7851acfb98c2c3aff69219af6770dbde469d264
streetartist <60282562+streetartist@users.noreply.github.com>
2021-02-03 11:50:49 +0800

    Update button.py

--- litchi/uix/button.py
+++ litchi/uix/button.py
@@ -3,4 +3,6 @@ class Button:
     def __init__(self, text, on_pressed, hold):
         self.text = text
     def convert(self):
-        return "<button>{self.text}</button>"
+        return '<button v-on:click="ButtonClick'+self.id+'">' + self.text + "</button>'
+    def text(self,change):
+        pass
