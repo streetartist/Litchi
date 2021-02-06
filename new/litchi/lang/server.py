2d8df75cc7372ba70afe90962e3f5c8e969a219e
streetartist <60282562+streetartist@users.noreply.github.com>
2021-02-06 11:58:28 +0800

    Update server.py

--- new/litchi/lang/server.py
+++ new/litchi/lang/server.py
@@ -1,4 +1,6 @@
 from litchi.lang.base import get_function_body
+from litchi.app import session
+
 import re
 
 def server(x):
@@ -7,7 +9,11 @@ def server(x):
 class Button:
     def __init__(self, function):
         self.function = function
-    def run(self):
+    def convert(self):
+        re.search("\s\S*\s*=\s*\s\S*",self.function)
+        for i in result.groups():
+            re.search("\w*",i)
+            self.function.replace(i,"session["+i+"])
         exec(
-            re.search(self.function,"\s\S*\s*=\s*\s\S*")
+            self.function
         )
