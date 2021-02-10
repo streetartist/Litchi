90b3ffc65c497b455fc3683b6d94dcb75fbb9e64
streetartist <60282562+streetartist@users.noreply.github.com>
2021-02-09 23:28:13 -0600

    Change Into a easy way.

--- new/litchi/lang/base.py
+++ new/litchi/lang/base.py
@@ -1,17 +1,7 @@
 import inspect
-import re
-from itertools import dropwhile
 
-def get_function_body(func):
-    # print()
-    # print("{func.__name__}'s body:".format(func=func))
-    source_lines = inspect.getsourcelines(func)[0]
-    source_lines = dropwhile(lambda x: x.startswith('@'), source_lines)
-    source = ''.join(source_lines)
-    pattern = re.compile(r'(async\s+)?def\s+\w+\s*\(.*?\)\s*:\s*(.*)', flags=re.S)
-    lines = pattern.search(source).group(2).splitlines()
-    if len(lines) == 1:
-        return lines[0]
-    else:
-        indentation = len(lines[1]) - len(lines[1].lstrip())
-        return '\n'.join([lines[0]] + [line[indentation:] for line in lines[1:]])
+def get_source(func):  
+    code=""
+    for line in inspect.getsourcelines(func)[0][2:]:
+        code+=line[4:]
+    return code
