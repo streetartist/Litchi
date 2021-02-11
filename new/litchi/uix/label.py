7945b5bcb54776afb5fb077518da021d37ac62c4
streetartist <60282562+streetartist@users.noreply.github.com>
2021-02-11 20:07:28 +0800

    Update label.py

--- new/litchi/uix/label.py
+++ new/litchi/uix/label.py
@@ -44,3 +44,50 @@ class H1:
             
     def text(self,change):
         return change
+
+
+class A:
+    def __init__(self, text, on_pressed, hold, id):
+        self.text = text
+        self.id = id
+        self.on_pressed = on_pressed
+    def convert(self):
+        return '''
+        <a id="{id}">{text}</a>
+        <script>
+            $('#{id}').click(function(){{
+                $.ajax({{
+                    url:"/ajax/{id}/click",
+                    type:"post",
+                    success:function(result){{
+                        $("#{id}").html(result);
+                }}}});
+            }});
+        </script>
+        '''.format(id=self.id, text=self.text)
+            
+    def text(self,change):
+        return change
+
+class Img:
+    def __init__(self, src, on_pressed, hold, id):
+        self.src = src
+        self.id = id
+        self.on_pressed = on_pressed
+    def convert(self):
+        return '''
+        <img id="{id}" src="{src}"></img>
+        <script>
+            $('#{id}').click(function(){{
+                $.ajax({{
+                    url:"/ajax/{id}/click",
+                    type:"post",
+                    success:function(result){{
+                        $("#{id}").html(result);
+                }}}});
+            }});
+        </script>
+        '''.format(id=self.id, src=self.src)
+            
+    def src(self,change):
+        return change
