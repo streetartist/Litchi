fff274a2201ceb6dca4a7d633dc9683133452154
streetartist <60282562+streetartist@users.noreply.github.com>
2021-03-19 09:06:40 -0500

    Update server.py

--- new/litchi/lang/server.py
+++ new/litchi/lang/server.py
@@ -3,6 +3,16 @@ from litchi.app import session, app
 
 import re
 
+'''
+
+            @app.route('/ajax/<id>/<event>', methods=['POST','GET'])
+            def ajax(id, event):
+                for element in elements:
+                    if element.id == id:
+                        if event == 'click':
+                            return jsonify(
+'''
+
 def server(func):
     function = get_source(func)
     re.search("\s\S*\s*=\s*\s\S*",function)
