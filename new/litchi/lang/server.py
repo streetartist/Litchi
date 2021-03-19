5600477601d9065552517d7ae0bd535bdf35324c
streetartist <60282562+streetartist@users.noreply.github.com>
2021-03-19 09:10:59 -0500

    Update server.py

--- new/litchi/lang/server.py
+++ new/litchi/lang/server.py
@@ -1,29 +1,13 @@
 from litchi.lang.base import get_source
-from litchi.app import session, app
-
-import re
-
-'''
-
-            @app.route('/ajax/<id>/<event>', methods=['POST','GET'])
-            def ajax(id, event):
-                for element in elements:
-                    if element.id == id:
-                        if event == 'click':
-                            return jsonify(
-'''
 
 def server(func):
     function = get_source(func)
-    re.search("\s\S*\s*=\s*\s\S*",function)
+
+    re.search("\s\S*\s*=\s*\s\S*",function) # 将变量定义加上session
+
     for i in result.groups():
         re.search("\w*",i)
         function.replace(i,"session["+i+"])
-    
-    @app()
-    def main():
-        pass
-    exec(
-        function
-    )
+
+    return function
         
