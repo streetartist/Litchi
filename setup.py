2a4f8e6a23f31c784be44140729c03d2434cd0eb
streetartist <60282562+streetartist@users.noreply.github.com>
2021-07-08 01:30:35 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from os import path
+DIR = path.dirname(path.abspath(__file__))
+INSTALL_PACKAGES = open(path.join(DIR, 'requirements.txt')).read().splitlines()
+with open(path.join(DIR, 'README.md')) as f:
+ README = f.read()
+setup(
+ name='litchi',
+ packages=['litchi'],
+ description="Build website with only Python",
+ long_description=README,
+ long_description_content_type='text/markdown',
+ install_requires=INSTALL_PACKAGES,
+ version='0.0.1',
+ url='http://github.com/streetartist/Litchi',
+ author='Streetartist',
+ author_email='15252635839@163.com',
+ keywords=['web', 'framework', 'flask'],
+ tests_require=[
+ 'pytest',
+ 'pytest-cov',
+ 'pytest-sugar'
+ ],
+ python_requires='>=3'
+)
