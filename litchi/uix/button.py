2be2968fbb127127ab8c55af57f800b5eba38def
streetartist <60282562+streetartist@users.noreply.github.com>
2021-07-10 19:34:19 +0800

    Delete litchi directory

--- litchi/uix/button.py
+++
@@ -1,8 +0,0 @@
-# Button code goes there.
-class Button:
-    def __init__(self, text, on_pressed, hold):
-        self.text = text
-    def convert(self):
-        return '<button v-on:click="ButtonClick'+self.id+'">' + self.text + "</button>'
-    def text(self,change):
-        pass
