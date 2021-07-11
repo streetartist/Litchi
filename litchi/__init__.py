50f143adb3943c779e0c7790fc99a66b322df51d
streetartist <60282562+streetartist@users.noreply.github.com>
2021-07-11 09:41:52 +0800

    Update __init__.py

--- litchi/__init__.py
+++ litchi/__init__.py
@@ -1,5 +1,1 @@
-from litchi.app import App
-from litchi.uix.button import Button
-from litchi.lang.brython import brython, Brython
-from litchi.uix.html import HTML
-from litchi.uix.label import A, H1, P, Img
+
