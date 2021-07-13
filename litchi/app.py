34f452000f16f68f574768080cc2cdb293f18062
streetartist <60282562+streetartist@users.noreply.github.com>
2021-07-13 10:23:22 +0000

    Update app.py

--- litchi/app.py
+++ litchi/app.py
@@ -3,8 +3,7 @@ from flask import Flask, jsonify, session
 
 def del_waste(self, wastes):
     waste = ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__',
-'__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', / 
-        'build', 'run', 'generate',]
+'__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'build', 'run', 'generate',]
     
     production = []
 
