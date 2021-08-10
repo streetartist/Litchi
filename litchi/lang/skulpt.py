26c96db50f39f1187738b23f744495d690d9ee58
streetartist <60282562+streetartist@users.noreply.github.com>
2021-08-10 02:58:39 +0000

    Update skulpt.py

--- litchi/lang/skulpt.py
+++ litchi/lang/skulpt.py
@@ -1,2 +1,57 @@
-class Skulpt:
-    
+class Brython:
+    def __init__(self, function):
+        self.function = function
+        self.litchi = None
+
+    def convert(self):
+        return '''
+    <script src="https://cdn.jsdelivr.net/npm/skulpt/dist/skulpt.min.js" type="text/javascript"></script>    
+    <script src="https://cdn.jsdelivr.net/npm/skulpt/dist/skulpt-stdlib.js" type="text/javascript"></script>
+
+    <script type="text/javascript">
+
+        function outf(text) {
+            var mypre = document.getElementById("output");  //
+            mypre.innerHTML = mypre.innerHTML + text;  //文字输出
+        }
+        function builtinRead(x) {
+            if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+                throw "File not found: '" + x + "'";
+            return Sk.builtinFiles["files"][x];
+        }
+         
+        function runit() {
+            console.log("runit is going")
+            var prog = document.getElementById("yourcode").value; //读取用户输入的内容
+            var mypre = document.getElementById("output");   //获取输出框
+            mypre.innerHTML = '';
+            Sk.pre = "output";
+            Sk.configure({ output: outf, read: builtinRead, __future__: Sk.python3 });
+ 
+            (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'mycanvas';
+            var myPromise = Sk.misceval.asyncToPromise(function () {
+                return Sk.importMainWithBody("<stdin>", false, prog, true);
+            });
+ 
+            myPromise.then(function (mod) {
+                console.log('success');    //执行成功，显示success
+            },
+                function (err) {
+                    console.log(err.toString());   //执行失败，在控制台显示 err
+                });
+        }
+    </script>
+    <form>
+        <!-- textarea是用户输入代码的部分，在此区域填写代码，每次运行都会显示 -->
+        <textarea id="yourcode" cols="80" rows="10">
+        </textarea><br />
+        <!-- 按钮：开始执行 -->
+        <button type="button" onclick="runit()">Run</button>
+    </form>
+    <!-- 文字输出部分 -->
+    <pre id="output"></pre>
+    <!-- 画图输出部分 -->
+    <div id="mycanvas"></div>
+</body>
+</html>
+        '''.format(code=self.function)
